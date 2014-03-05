9685dec463cb81962f1e1e5af4f3e557e8c2320c
Denislav Parvanov <denislav_1995@abv.bg>
2014-03-06 01:12:43 +0200

    changes to add_diploma_works in committee

--- diplomirane/app/models/committee.rb
+++ diplomirane/app/models/committee.rb
@@ -3,4 +3,24 @@ class Committee < ActiveRecord::Base
 	has_many :diploma_works
 	belongs_to :president, class_name: "Teacher", foreign_key: "president_id"
 	validates :date, :presence => true, :uniqueness => true
+
+	def tags
+		@tags = Array.new
+		self.commissioners.each do |commissioner|
+			commissioner.tags.each do |tag|
+				if !@tags.include?(tag)
+					@tags << tag
+				end	
+			end	
+		end	
+		@tags
+	end
+
+	def check_tags(diploma_work)
+		@committee_tags = self.tags.map(&:name)
+		@diploma_work_tags = diploma_work.tags.map(&:name)
+		@intersection = (@committee_tags & @diploma_work_tags)
+		@intersection.length
+	end	
+
 end
